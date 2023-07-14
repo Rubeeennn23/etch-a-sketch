8406b2d46a413548667919ac6f4c610b6c306e68
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-07-14 18:14:29 +0200

    creating the grid and trying hoving

--- code.js
+++ code.js
@@ -1,4 +1,29 @@
-const div = document.createElement('div');
-div.querySelector('div')
-div.classList.add('grid');
-div.textContent = 'Hello';
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.querySelector('div')
+container.classList.toggle('grid');
+
+for (let i=1; i<=256; i++) {
+    let box= document.createElement('div');
+    container.appendChild(box);
+    box.querySelector('div');
+    box.classList.toggle('block')
+   
+
+}
+const box = document.querySelector('.block')
+const boxes = document.querySelectorAll('.block')
+boxes.forEach(div => {div.addEventListener("mouseover", mouseOver)});
+boxes.forEach(div => {div.addEventListener("mouseout", mouseOut)});
+
+function mouseOver() {
+    box.style.background = "red";
+}
+
+function mouseOut() {
+    box.style.background = "black";
+}
+
+for (let x=1;x<=256;x++) {
+    
+}
