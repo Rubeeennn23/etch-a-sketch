9dc9fef7f3f0a530dacde30bae53029945922eb8
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-07-18 18:41:16 +0200

    create a function to create a grid

--- code.js
+++ code.js
@@ -9,34 +9,39 @@ document.body.appendChild(container);
 container.querySelector('div')
 container.classList.toggle('grid');
 
-let boxNumber=16;
-let squaresWidth = 480/boxNumber;
-let squaresHeight = 480/boxNumber;  
+let boxNumber=32;
+createGrid(boxNumber);
 
+function createGrid(boxNumber) {
+    let gridSize= boxNumber*boxNumber;
+    let squaresWidth = 480/boxNumber;
+    let squaresHeight = 480/boxNumber;  
+for (let i=1; i<=gridSize; i++) {
+        function mouseOver() {
+            box.style.background = "red"
+        };
+        
+        function mouseOut() {
+            box.style.background = "black"
+        };
 
-for (let i=1; i<=256; i++) {
-    function mouseOver() {
-        box.style.background = "red"
-    };
-    
-    function mouseOut() {
-        box.style.background = "black"
-    };
-
-    let box= document.createElement('div');
-    container.appendChild(box);
-    box.querySelector('div');
-    box.classList.toggle('block');
-    box.addEventListener("mouseover", mouseOver);
-    box.addEventListener("mouseout", mouseOut);
-    box.style.cssText = `width: ${squaresWidth}px; height:${squaresHeight}px;`
+        let box= document.createElement('div');
+        container.appendChild(box);
+        box.querySelector('div');
+        box.classList.toggle('block');
+        box.addEventListener("mouseover", mouseOver);
+        box.addEventListener("mouseout", mouseOut);
+        box.style.cssText = `width: ${squaresWidth}px; height:${squaresHeight}px;`
+    }
 };
 
 function gridChange() {
     let squaresNumber = parseInt(prompt('Select a grid number of squares per row and column'))
     if (squaresNumber>0 && squaresNumber<=100) {
         boxNumber=squaresNumber
+        return createGrid(boxNumber)
     } else {
         return alert('ERROR: The number of squares has to be between 1 and 100')
-    } return boxNumber;
+    }
+    
 };
